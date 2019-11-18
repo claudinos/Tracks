fdcb2f0ca9dfff5abacedbbfbca68529a2b73997
Tona <tonadaphine77@gmail.com>
2019-11-18 09:56:19 +0200

    Complete Dashboard

--- app/src/main/java/com/moringa/tracks/MainActivity.java
+++ app/src/main/java/com/moringa/tracks/MainActivity.java
@@ -1,14 +1,32 @@
 package com.moringa.tracks;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawLayout =(DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,mDrawLayout,R.string.open,R.string.close);
+        mDrawLayout.addDrawerListener(mToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
